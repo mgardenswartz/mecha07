dabd4fd0141f5abb59aa47523c76f4a9b9379d6b
Max Gardenswartz <134117510+mgardenswartz@users.noreply.github.com>
2023-12-02 16:08:56 -0800

    Update

--- BNO055_Driver.py
+++ BNO055_Driver.py
@@ -22,7 +22,15 @@ class BNO055_Driver:
         self.gyr_calib_mask = 0b0011_0000
         self.acc_calib_mask = 0b0000_1100
         self.mag_calib_mask = 0b0000_0011
-
+        self.axis_placement_mapping = {
+                    "P0": (0x21, 0x04),
+                    "P1": (0x24, 0x00),
+                    "P2": (0x24, 0x06),
+                    "P3": (0x21, 0x02),
+                    "P4": (0x24, 0x03),
+                    "P5": (0x21, 0x01),
+                    "P6": (0x21, 0x07),
+                    "P7": (0x24, 0x05)}
         # Files
         self.file_name = "IMU_cal_coeffs.txt"
         self.files = os.listdir()
@@ -82,7 +90,7 @@ class BNO055_Driver:
         else:
             self.mag_cal = False
 
-        return [self.sys_cal,self.gyr_cal,self.acc_cal,self.mag_cal]
+        return self.sys_cal,self.gyr_cal,self.acc_cal,self.mag_cal
     
     def retrieve_coefficients(self):
         self.coefficients_to_read = bytearray(22)
@@ -97,7 +105,24 @@ class BNO055_Driver:
         return self.results
         
     def write_coefficients(self,coefficients: list[int]):
-        self.i2c.mem_write()
+        self.coefficients_to_write = coefficients
+
+        # Test for valid length.
+        if len(self.coefficients_to_write) != 22:
+            raise ValueError("Input does not contain 22 integers.")
+        
+        # Are all elements in range of a 16-bit sign integer?
+        if any(element < -32768 or element > 32767 for element in self.coefficients_to_write):
+            raise ValueError("At least one element is out of range for a 16-bit number.")
+        
+        # Convert to hex.
+        self.coefficients_to_write_hex = [hex(element) for element in self.coefficients_to_write]
+
+        # Separate into twice as many 8-bytes and write to registers.
+        for index in range(0,11):
+            self.MSB,self.LSB = self.separate_16bit_hex(self.coefficients_to_write_hex[index])
+            self.i2c.mem_write(self.MSB, addr = 0x6A - 2*index, timeout = self.timeout)
+            self.i2c.mem_write(self.LSB, addr = 0x69 - 2*index, timeout = self.timeout)
 
     def read_Euler_angles(self):
         self.raw_Euler_angles = bytearray(6)
@@ -130,34 +155,14 @@ class BNO055_Driver:
 
         return self.gyr_data
 
-    def configure_coordinate_system(self,axis_placement: str):
+    def configure_coordinate_system(self, axis_placement: str):
         self.axis_placement = axis_placement
-        if self.axis_placement == "P0":
-            self.axis_map_config = 0x21
-            self.axis_map_sign = 0x04
-        elif self.axis_placement == "P1":
-            self.axis_map_config = 0x24
-            self.axis_map_sign = 0x00
-        elif self.axis_placement == "P2":
-            self.axis_map_config = 0x24
-            self.axis_map_sign = 0x06
-        elif self.axis_placement == "P3":
-            self.axis_map_config = 0x21
-            self.axis_map_sign = 0x02
-        elif self.axis_placement == "P4":
-            self.axis_map_config = 0x24
-            self.axis_map_sign = 0x03
-        elif self.axis_placement == "P5":
-            self.axis_map_config = 0x21
-            self.axis_map_sign = 0x01
-        elif self.axis_placement == "P6":
-            self.axis_map_config = 0x21
-            self.axis_map_sign = 0x07
-        elif self.axis_placement == "P7":
-            self.axis_map_config = 0x24
-            self.axis_map_sign = 0x05
+        if self.axis_placement in self.axis_placement_mapping:
+            self.axis_map_config, self.axis_map_sign = self.axis_placement_mapping[self.axis_placement]
         else:
-            raise ValueError("Invalid axis_placement value. Check the input.")
+            # Handle the case where the provided axis_placement is not in the mapping
+            print(f"Invalid axis_placement: {self.axis_placement}")
+            # You might want to raise an exception or handle it in some other way
 
         # Send info
         self.i2c.mem_write(self.axis_map_config,
@@ -167,3 +172,17 @@ class BNO055_Driver:
                            addr = self.axis_map_sign_addr,
                            timeout = self.timeout)
         
+
+    def separate_16bit_hex(hex_number):
+        # Ensure the input is a valid hexadecimal string
+        if not isinstance(hex_number, str) or not hex_number.startswith("0x"):
+            raise ValueError("Input must be a 16-bit hexadecimal string starting with '0x'")
+
+        # Remove '0x' prefix and convert to an integer
+        num = int(hex_number, 16)
+
+        # Separate into two 8-bit hex numbers
+        high_byte = (num >> 8) & 0xFF
+        low_byte = num & 0xFF
+
+        return high_byte,low_byte
